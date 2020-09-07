fccd10dd778b7bb5f2588d894fcd6d275f1dab25
Caleb <calebkim32@gmail.com>
2020-09-06 19:54:36 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>SASPAF</h1>
-    </div>
-  );
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <ul>
+            <li>Home</li>
+            <li>GetInvolved</li>
+            </ul>
+            {this.props.children}
+         </div>
+      )
+   }
 }
 
-export default App;
+class Home extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Home</h1>
+         </div>
+      )
+   }
+}
+export default {App, Home};
+
+ReactDOM.render((
+  <Router history = {browserHistory}>
+     <Route path = "/" component = {App}>
+        <IndexRoute component = {Home} />
+        <Route path = "Home" component = {Home} />
+        <Route path = "Get Involved" component = {GetInvolved} />
+     </Route>
+  </Router>
+), document.getElementById('app'))
---
+++ src/components/pages/GetInvolved.js
@@ -0,0 +1,10 @@
+class GetInvolved extends React.Component {
+    render() {
+       return (
+          <div>
+             <h1>Get Involved</h1>
+          </div>
+       )
+    }
+ }
+ export default GetInvolved;
